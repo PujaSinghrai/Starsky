60c84ee5c09f51816dde9319ccbfaac541e39bc1
pujasinghrai <pujasingh4rai@gmail.com>
2021-04-28 18:51:36 +0530

    arr

--- File/sp.js
+++ File/sp.js
@@ -1,4 +1,10 @@
 let user = { name: "Kavitha", email: "greetlabs@gmail.com" };
-let updated_User = { ...user, password: "123ABC" };
+let arr_User = { ...user, password: "123ABC" };
 
-console.log(updated_User);
+let new_user_details = { ...arr_User, 
+                     location:"kolkata", password:"234FDG"};
+
+
+console.log(arr_User);
+
+console.log(new_user_details);
